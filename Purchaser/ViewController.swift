65dc314da5a7caf8a1b33df2a408d7cbf2e76808
Luis Cardenas <lcardenas@doonamis.es>
2018-11-27 11:59:20 +0100

    Initial Commit

---
+++ Purchaser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Purchaser
+//
+//  Created by Luis Cardenas on 27/11/2018.
+//  Copyright © 2018 ThXou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
